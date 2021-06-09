b2159e181d42fda0132da740cb7518229dac3cb6
soimmary <55881643+soimmary@users.noreply.github.com>
2021-06-10 00:25:13 +0300

    Update grammarly.py

--- grammarly.py
+++ grammarly.py
@@ -1,10 +1,11 @@
+import os
+import time
+import csv
+from tqdm import tqdm
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
-from tqdm import tqdm
-import time
-import os
-import csv
 
 import personal_info
 
@@ -32,10 +33,7 @@ def get_level(folder_path):
     open_new_file = driver.find_element_by_xpath("//div[@data-name='new-doc-add-btn']").click()
     time.sleep(2)
 
-    a = ['MTsy_14_1.txt', 'MTsy_16_1.txt', 'MTsy_17_1.txt', 'MTsy_17_2.txt', 'MTsy_18_1.txt', 'MTsy_18_2.txt', 'MTsy_19_1.txt', 'MTsy_19_2.txt', 'MTsy_20_1.txt', 'MTsy_20_2.txt', 'MTsy_21_1.txt', 'MTsy_21_2.txt', 'MTsy_22_1.txt', 'MTsy_22_2.txt', 'MTsy_23_1.txt', 'MTsy_23_2.txt', 'MTsy_24_1.txt', 'MTsy_24_2.txt', 'MTsy_25_1.txt', 'MTsy_25_2.txt', 'MTsy_26_1.txt', 'MTsy_26_2.txt', 'MTsy_27_1.txt', 'MTsy_27_2.txt', 'MTsy_28_1.txt', 'MTsy_28_2.txt', 'MTsy_29_1.txt', 'MTsy_29_2.txt', 'MTsy_30_1.txt', 'MTsy_30_2.txt', 'MTsy_31_1.txt', 'MTsy_31_2.txt', 'MTsy_32_1.txt', 'MTsy_32_2.txt', 'MTsy_33_1.txt', 'MTsy_33_2.txt', 'MTsy_34_1.txt', 'MTsy_34_2.txt', 'MTsy_35_1.txt', 'MTsy_35_2.txt', 'MTsy_36_1.txt', 'MTsy_36_2.txt', 'MTsy_37_1.txt', 'MTsy_37_2.txt', 'MTsy_38_1.txt', 'MTsy_38_2.txt', 'MTsy_39_1.txt', 'MTsy_39_2.txt', 'MTsy_40_1.txt', 'MTsy_40_2.txt', 'MTsy_41_1.txt', 'MTsy_41_2.txt', 'MTsy_42_1.txt', 'MTsy_42_2.txt', 'MTsy_43_1.txt', 'MTsy_43_2.txt', 'MTsy_7_2.txt', 'MTsy_8_1.txt', 'MTsy_8_2.txt', 'MTsy_9_1.txt', 'TSha_2_1.txt', 'TSha_2_2.txt', 'TSha_20_1.txt', 'TSha_20_2.txt', 'TSha_21_1.txt', 'TSha_21_2.txt', 'TSha_4_1.txt', 'TSha_4_2.txt', 'TSha_6_1.txt', 'TSha_6_2.txt', 'TSha_7_1.txt', 'TSha_7_2.txt', 'TSha_9_1.txt', 'TSha_9_2.txt', 'VKo_1_1.txt', 'VKo_1_2.txt', 'VKo_12_2.txt', 'VKo_13_2.txt', 'VKo_16_2.txt', 'VKo_17_1.txt', 'VKo_17_2.txt', 'VKo_18_2.txt', 'VKo_2_2.txt', 'VKo_20_2.txt', 'VKo_21_2.txt', 'VKo_23_1.txt', 'VKo_24_2.txt', 'VKo_25_2.txt', 'VKo_28_1.txt', 'VKo_29_2.txt', 'VKo_3_2.txt', 'VKo_32_1.txt', 'VKo_33_1.txt', 'VKo_33_2.txt', 'VKo_34_2.txt', 'VKo_4_2.txt', 'VKo_6_1.txt', 'VKo_7_1.txt', 'VKo_9_2.txt', 'VPe_12_1.txt', 'VPe_12_2.txt', 'VPe_17_1.txt', 'VPe_17_2.txt', 'VPe_18_1.txt', 'VPe_18_2.txt', 'VPe_19_1.txt', 'VPe_19_2.txt', 'VPe_20_1.txt', 'VPe_20_2.txt', 'VPe_21_1.txt', 'VPe_21_2.txt', 'VPe_22_1.txt', 'VPe_22_2.txt', 'VPe_23_1.txt', 'VPe_23_2.txt', 'VPe_24_1.txt', 'VPe_24_2.txt', 'VPe_25_1.txt', 'VPe_25_2.txt', 'VPe_26_1.txt', 'VPe_26_2.txt', 'VPe_27_1.txt', 'VPe_27_2.txt', 'VPe_28_1.txt', 'VPe_28_2.txt', 'VPe_30_1.txt', 'VPe_30_2.txt', 'VPe_31_1.txt', 'VPe_31_2.txt', 'VPe_32_1.txt', 'VPe_32_2.txt', 'VPe_33_1.txt', 'VPe_33_2.txt', 'VPe_34_2.txt', 'VPe_35_1.txt', 'VPe_35_2.txt', 'VPe_36_1.txt', 'AAl_14_2.txt', 'KKo_24_1.txt']
-
-    #for path in tqdm(os.listdir(folder_path)):
-    for path in tqdm(a):
+    for path in tqdm(os.listdir(folder_path)):
         with open('grammarly_results.csv', 'a') as csvf:
             writer = csv.writer(csvf, delimiter=',')
             with open(folder_path + '/' + path, 'r') as f:
@@ -44,7 +42,7 @@ def get_level(folder_path):
                 insert_text = driver.find_element_by_xpath("//div[@class='_9c5f1d66-denali-editor-editor ql-editor ql-blank']").send_keys(text)
                 time.sleep(15)
                 score = int(driver.find_element_by_xpath("//div[@class='fhsusol _bec19051-header-performanceScoreFadeIn _48adf116-header-performanceScore']").text)
-                '''if score in range(0, 6):
+                if score in range(0, 6):
                     level = 'A1'
                 elif score in range(6, 11):
                     level = 'A2'
@@ -55,10 +53,10 @@ def get_level(folder_path):
                 elif score in range(51, 71):
                     level = 'C1'
                 elif score in range(71, 101):
-                    level = 'C2'''
+                    level = 'C2'
                 start_again = driver.find_element_by_xpath('//div[@class="_9c5f1d66-denali-editor-editor ql-editor"]').clear()
-                writer.writerow([path, score])
+                writer.writerow([path, level, score])
             csvf.close()
 
 
-get_level('REALEC_texts')
+get_level('/PycharmProjects/REALEC/REALEC_texts')
